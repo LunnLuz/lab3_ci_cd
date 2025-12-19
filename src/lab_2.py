5915513add660fa18a9423b63731479b66e902f3
LunnLuz <khas_ink@mail.ru>
2025-12-19 10:34:36 +0500

    Lab_3_actions

---
+++ src/lab_2.py
@@ -0,0 +1,42 @@
+class Lab_2:
+    def Fibonacci(n):
+        if n < 1:
+            return []
+        elif n == 1:
+            return [0]
+        elif n == 2:
+            return [0,1]
+        else:
+            fibonacci = [0,1]
+            for i in range(2,n):
+                fib_next = fibonacci[i-1]+fibonacci[i-2]
+                fibonacci.append(fib_next)
+            return fibonacci
+    
+    def BubbleSort(N):
+        if len(N) == 0:
+            return []
+        for i in range(len(N)):
+            for j in range(len(N)-i-1):
+                if N[j]>N[j+1]:
+                    N[j], N[j+1] = N[j+1], N[j]
+        return N
+    
+    def EratosthenSieve(n):
+        if n < 2:
+            return []
+        list_erotosthen = [1] * (n+1)
+        list_erotosthen[0] = list_erotosthen[1] = 0
+        erotosthen_exsi = []
+    
+        p = 2
+        while p ** 2 <= n:
+            if list_erotosthen[p]:
+                for i in range(p ** 2, n + 1, p):
+                    list_erotosthen[i] = 0
+            p += 1
+    
+        for i in range(2,n+1):
+            if list_erotosthen[i]:
+                erotosthen_exsi.append(i)
+        return erotosthen_exsi
---
+++ tests/test.py
@@ -0,0 +1,67 @@
+import pytest, sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__),'..','src'))
+from lab_2 import Lab_2
+
+class TextLab:
+    def test_fibonacci_negative():
+        """n < 1 — граничное значение"""
+        assert Fibonacci(-5) == []
+        assert Fibonacci(0) == []
+
+    def test_fibonacci_boundary():
+        """Граничные значения"""
+        assert Fibonacci(1) == [0]
+        assert Fibonacci(2) == [0, 1]
+
+    def test_fibonacci_normal():
+        """Корректные значения n > 2"""
+        assert Fibonacci(5) == [0, 1, 1, 2, 3]
+        assert Fibonacci(7) == [0, 1, 1, 2, 3, 5, 8]
+
+    def test_fibonacci_invalid_type():
+        """Некорректный тип данных"""
+        with pytest.raises(TypeError):
+            Fibonacci("abc")
+
+
+    def test_bubblesort_empty():
+        """Пустой список"""
+        assert BubbleSort([]) == []
+
+    def test_bubblesort_sorted():
+        """Уже отсортированный список"""
+        assert BubbleSort([1, 2, 3]) == [1, 2, 3]
+
+    def test_bubblesort_unsorted():
+        """Неотсортированный список"""
+        assert BubbleSort([3, 1, 2]) == [1, 2, 3]
+
+    def test_bubblesort_negative_numbers():
+        """Список с отрицательными числами"""
+        assert BubbleSort([0, -1, -5, 2]) == [-5, -1, 0, 2]
+
+    def test_bubblesort_invalid_type():
+        """Некорректный тип данных"""
+        with pytest.raises(TypeError):
+            BubbleSort("1230")
+
+
+    def test_eratosthen_negative():
+        """n < 2 — граничные значения"""
+        assert EratosthenSieve(-10) == []
+        assert EratosthenSieve(0) == []
+        assert EratosthenSieve(1) == []
+
+    def test_eratosthen_boundary():
+        """Граничное значение"""
+        assert EratosthenSieve(2) == [2]
+
+    def test_eratosthen_normal():
+        """Корректные входные данные"""
+        assert EratosthenSieve(10) == [2, 3, 5, 7]
+        assert EratosthenSieve(20) == [2, 3, 5, 7, 11, 13, 17, 19]
+
+    def test_eratosthen_invalid_type():
+        """Некорректный тип данных"""
+        with pytest.raises(TypeError):
+            EratosthenSieve("abc")
